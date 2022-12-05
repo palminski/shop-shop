87a33d2958f91d37bc2eac997b8d6f6b38bff982
palminski <williambolls98@gmail.com>
2022-12-05 10:20:12 -0800

    finished stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,9 +8,18 @@ import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 
 import { idbPromise } from "../../utils/helpers";
 
+import {QUERY_CHECKOUT} from '../../utils/queries';
+import {loadStripe} from '@stripe/stripe-js';
+
+import { useLazyQuery } from "@apollo/client";
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
     
     const [state, dispatch] = useStoreContext();
+
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -23,6 +32,14 @@ const Cart = () => {
         }
     }, [state.cart.length, dispatch]);
 
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({sessionId: data.checkout.session});
+            });
+        }
+    }, [data]);
+
     function toggleCart() {
         dispatch({type: TOGGLE_CART});
     }
@@ -33,6 +50,20 @@ const Cart = () => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: {products: productIds}
+        });
     }
 
     if (!state.cartOpen) {
@@ -57,7 +88,7 @@ const Cart = () => {
                             <strong>Total: ${calculateTotal()}</strong>
                             {
                                 Auth.loggedIn() ? 
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
